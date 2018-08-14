3b3456db3320c60e89544cc0ba650632cd18813e
jeaa28 <42361670+jeaa28@users.noreply.github.com>
2018-08-13 20:37:29 -0500

    Add files via upload

---
+++ ProyectoHola.c
@@ -0,0 +1,20 @@
+/**main.c - Este es un archivo Hola Mundo
+*incluido en este curso --solo para evitar
+*la maldici\'on del programador--. En este curso, 
+*usted aprender\'a a usar correctamente muchos de
+*los elementos del lenguaje C. Especialmente, usted
+*deber\'a a\'render a usar apuntadores, de lo contrario lo
+*sufrir\'a lo indecible y un perro salvaje lo
+*preseguir\'a por los pasillos de la UPIITA
+#include <stdio.h> /*printf(const char*, ...)*/
+#include <stdlib.h>
+
+int main(int argc, char *argv[])
+{
+ putchar('A'); 
+ putchar('\n');
+ putchar('\n');
+ printf("HOLA MUNDO C!!!\n");
+  system("PAUSE");	
+  return 0;
+}
